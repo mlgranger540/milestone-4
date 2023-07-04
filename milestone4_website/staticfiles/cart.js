1091cb2a6537e85bc839d046e1bf7237f223e418
mlgranger540 <mattgranger540@outlook.com>
2023-07-05 00:17:51 +0100

    Another minor change to cart.js

--- milestone4_website/staticfiles/cart.js
+++ milestone4_website/staticfiles/cart.js
@@ -74,7 +74,6 @@ fetch("/orders/config/").then((result) => { return result.json(); }).then((data)
             return result.json();
         })
         .then((data) => {
-            console.log(data);
             return stripe.redirectToCheckout({sessionId: data.sessionId});
         })
     });
