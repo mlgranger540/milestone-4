f39bae4546587c3a09af7e8856406fcaa27e39ba
mlgranger540 <mattgranger540@outlook.com>
2023-07-21 17:16:00 +0100

    Add WhiteNoise middleware to serve static files in production

--- milestone4_website/settings.py
+++ milestone4_website/settings.py
@@ -60,6 +60,7 @@ AUTH_USER_MODEL = "users.User"  # new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
