438c8ececb8a6ca7c9bcdef12ab18364cc7c00da
mlgranger540 <mattgranger540@outlook.com>
2023-07-26 21:51:27 +0100

    Hide production secret key in .env and turn off debug in Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --chdir ./milestone4_website milestone4_website.wsgi --log-file - --log-level debug
+web: gunicorn --chdir ./milestone4_website milestone4_website.wsgi
--- milestone4_website/settings.py
+++ milestone4_website/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m8j%kp#6r%9jyo7jwo8wr67n_^kwbef-t#m^^)_yfm(e1o12d$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
